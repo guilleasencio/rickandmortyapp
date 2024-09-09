e72b288643c3da4cec10b4a6b06f1722613c86ff
Guillermo Asencio Sanchez <guillermo.asencio.sanchez@gmail.com>
2024-09-09 13:32:18 +0200

    Created project

---
+++ alkimiirickandmorty/alkimiirickandmorty/alkimiirickandmortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  alkimiirickandmortyApp.swift
+//  alkimiirickandmorty
+//
+//  Created by Guillermo Asencio Sanchez on 9/9/24.
+//
+
+import SwiftUI
+
+@main
+struct alkimiirickandmortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
