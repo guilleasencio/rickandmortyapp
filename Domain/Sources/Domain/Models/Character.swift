5fa6e143aae542ec3e6fc6726c50ca33ab4f2b99
Guillermo Asencio Sanchez <guillermo.asencio.sanchez@gmail.com>
2024-09-10 17:38:15 +0200

    Update Domain models to have a public init

--- Domain/Sources/Domain/Models/Character.swift
+++ Domain/Sources/Domain/Models/Character.swift
@@ -13,6 +13,13 @@ public struct Character {
         case dead = "Dead"
         case unknown
     }
+    
+    public static func getStatus(_ from: String?) -> Status {
+        guard let from = from, let status = Status(rawValue: from) else {
+            return .unknown
+        }
+        return status
+    }
 
     public enum Gender: String {
         case female = "Female"
@@ -21,15 +28,48 @@ public struct Character {
         case unknown
     }
     
-    public let id: Int
+    public static func getGender(_ from: String?) -> Gender {
+        guard let from = from, let gender = Gender(rawValue: from) else {
+            return .unknown
+        }
+        return gender
+    }
+    
+    public let id: String
     public let name: String
-    public let status: Status
-    public let species: String
-    public let type: String
+    public let status: Status?
+    public let species: String?
+    public let type: String?
     public let gender: Gender
-    public let origin: Location
-    public let location: Location
-    public let image: String
+    public let origin: Location?
+    public let location: Location?
+    public let image: String?
     public let episode: [Episode]
-    public let created: Date
+    public let created: String?
+    
+    public init(
+        id: String,
+        name: String,
+        status: Status?,
+        species: String?,
+        type: String?,
+        gender: Gender,
+        origin: Location?,
+        location: Location?,
+        image: String?,
+        episode: [Episode],
+        created: String?
+    ) {
+        self.id = id
+        self.name = name
+        self.status = status
+        self.species = species
+        self.type = type
+        self.gender = gender
+        self.origin = origin
+        self.location = location
+        self.image = image
+        self.episode = episode
+        self.created = created
+    }
 }
--- Domain/Sources/Domain/Models/Episode.swift
+++ Domain/Sources/Domain/Models/Episode.swift
@@ -9,13 +9,32 @@ import Foundation
 
 public struct Episode {
     public struct Character {
-        public let id: Int
+        public let id: String
         public let name: String
+        
+        public init(id: String, name: String) {
+            self.id = id
+            self.name = name
+        }
     }
     
-    public let id: Int
-    public let name: String
-    public let airDate: String
+    public let id: String
+    public let name: String?
+    public let airDate: String?
     public let characters: [Character]
-    public let created: Date
+    public let created: String?
+    
+    public init(
+        id: String,
+        name: String?,
+        airDate: String?,
+        characters: [Character],
+        created: String?
+    ) {
+        self.id = id
+        self.name = name
+        self.airDate = airDate
+        self.characters = characters
+        self.created = created
+    }
 }
